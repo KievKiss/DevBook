af7ab0187574f258ffafb8d410ea7908177d376f
kiev <kevinkiss2010@gmail.com>
2024-10-30 15:29:50 -0300

    Criando modelo de usuario, e arquivo sql

---
+++ DevBook/api/src/modelos/usuario.go
@@ -0,0 +1,13 @@
+package modelos
+
+import "time"
+
+// Usuario representa os dados de um usúario
+type Usuario struct {
+	ID       uint64    `json:"id,omitempty"`
+	Nome     string    `json:"nome,omitempty"`
+	Nick     string    `json:"nick,omitempty"`
+	Email    string    `json:"email,omitempty"`
+	Senha    string    `json:"senha,omitempty"`
+	CriadoEM time.Time `json:"criadoEm,omitempty"`
+}
